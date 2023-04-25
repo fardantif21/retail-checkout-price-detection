695631f9b73f7dcbce19698af6b905bbde203045
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-04-25 17:08:16 +0700

    add view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,23 @@
 <?php
 
-class About {
-    public function index($p1 = 'Default 1', $p2 = 'Default 2') {
-        echo "param1 = $p1, param2 = $p2";
+class About extends Controller {
+    public function index($param1 = 'Default 1', $param2 = 'Default 2') {
+
+        $data['judul'] = 'About';
+        $data['param1'] = $param1;
+        $data['param2'] = $param2;
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+
     }
 
     public function page() {
-        echo 'about/index';
+
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About</h1>
+    <p>Parameter 1 = <?= $data['param1']; ?>, Parameter 2 = <?= $data['param2']; ?></p>
