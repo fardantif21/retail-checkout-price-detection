fddaa39415ed7c2f0d1086153460981974b0b68c
moaibad <fathulibad.412@gmail.com>
2023-04-29 13:11:17 +0700

    update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,11 +3,14 @@
 
 <?php
 // Menjalankan skrip Python dan menangkap outputnya sebagai string
-$output = shell_exec("python C:\\xampp\htdocs\\retail-checkout-price-detection\yolo\opencv.py");
-echo $output;
+//$output = shell_exec("python C:\\xampp\htdocs\\retail-checkout-price-detection\yolo\opencv.py");
+//echo $output;
 
 // Mengubah string JSON menjadi array PHP
-$arr = json_decode($output);
+//$arr = json_decode($output);
+
+$arr = ["Lifebuoy Total Protect Soap 96gm", "Fanta 250ml", "LU Oreo Biscuit 19gm"];
+
 var_dump($arr)
 
 ?>
