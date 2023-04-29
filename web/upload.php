64ee8363787ce02d68794606762b43c9d87a90be
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-04-29 14:40:16 +0700

    update upload.php

--- web/upload.php
+++ web/upload.php
@@ -3,7 +3,7 @@ $folder="../yolo/";
 if (!file_exists($folder)) {
    mkdir($folder, 0777);
 }
-$move = move_uploaded_file($_FILES["upload_file"]["tmp_name"], $folder . "/" . $_FILES["upload_file"]["name"]);
+$move = move_uploaded_file($_FILES["upload_file"]["tmp_name"], $folder . "/" . "tes.jpg");
 if($move){
    echo "File uploaded successfully..";
 }else{
