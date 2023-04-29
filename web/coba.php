d8587273f2eb0002b93576de135f4ec9210923a8
moaibad <fathulibad.412@gmail.com>
2023-04-29 17:03:54 +0700

    update database

--- web/coba.php
+++ web/coba.php
@@ -2,7 +2,7 @@
 	$conn = mysqli_connect("localhost","root","","data");
     
 	//Query sql
-	$query="SELECT nama FROM produk";
+	//$query="SELECT nama FROM produk";
     
 // Menjalankan skrip Python dan menangkap outputnya sebagai string
 //$output = shell_exec("python C:\\xampp\htdocs\\retail-checkout-price-detection\yolo\opencv.py");
@@ -14,23 +14,33 @@
 $arr = ["Lifebuoy Total Protect Soap 96gm", "Fanta 250ml", "LU Oreo Biscuit 19gm"];
 
 // Membuat string kondisi SQL untuk mencocokkan nilai dalam array
-$nilai_sql = "'" . implode("', '", $arr) . "'";
+//$nilai_sql = "'" . implode("', '", $arr) . "'";
 
-// Query SQL untuk mengambil harga produk dengan warna tertentu dalam array
-$query = "SELECT harga FROM produk WHERE nama IN ($nilai_sql)";
 
-// Menjalankan query dan menyimpan hasilnya dalam variabel $result
-$result = mysqli_query($conn, $query);
+foreach ($arr as $item) {
+    echo $item . "<br>";
+  }
+
+  
+foreach ($arr as $item) {
+    //query untuk setiap iterasi
+    $query = "SELECT * FROM produk WHERE nama='$item'";
+    $result = mysqli_query($conn, $query);
+  
+    //proses hasil query
+    while ($row = mysqli_fetch_assoc($result)) {
+      //tampilkan data hasil query
+      echo $row['nama'] . " " . $row['harga'] . "<br>";
+    }
+  }
 
 // Menampilkan hasil query
-while ($row = mysqli_fetch_assoc($result)) {
-    $harga = $row["harga"];
-    echo "Harga produk dengan nama " . $arr[array_search($row['nama'], $arr)] . " adalah: " . $harga . "<br>";
-    $total=$harga;
+// while ($row = mysqli_fetch_assoc($result)) {
+//     $harga = $row["harga"];
+//     echo "Harga produk dengan nama " . $arr[array_search($row['nama'], $arr)] . " adalah: " . $harga . "<br>";
+//     $total=$harga;
     
-}
-echo "Total belanjaan :" .$total. "<br>";
-
-
+// }
+// echo "Total belanjaan :" .$total. "<br>";
 
 ?>
